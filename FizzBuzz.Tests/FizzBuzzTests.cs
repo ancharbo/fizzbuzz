1867bdca5af853f5df9ce0c2cf590a440d7c35e1
Andrew Charbonneau <ancharbo@microsoft.com>
2020-02-17 15:37:43 -0500

    setup instance of service and first default test - with saves :(

--- FizzBuzz.Tests/FizzBuzzTests.cs
+++ FizzBuzz.Tests/FizzBuzzTests.cs
@@ -6,6 +6,20 @@ namespace FizzBuzz.Tests
     [TestClass]
     public class FizzBuzzTests
     {
+        //have test class setup instance of FizzBuzz Service
+        private FizzBuzzService _fizzbuzz;
+        public FizzBuzzTests()
+        {
+            _fizzbuzz = new FizzBuzzService();
+        }
 
+
+        //Start with test for getting default behavior of returning the number that we give to the service.
+        [TestMethod]
+
+        public void ShouldPrintNumber()
+        {
+            Assert.AreEqual("1",_fizzbuzz.Print(1));
+        }
     }
 }
